8e8eb9d95fc7a38bbfc4297a1f0f5825cd0f1bd5
chris <kaz71@live.com.mx>
2025-03-23 17:44:34 -0600

    Subimos la función de creado de salas sync con realtime DB y eliminamos warnings

--- app/src/main/java/com/kaz/playlistify/MainActivity.kt
+++ app/src/main/java/com/kaz/playlistify/MainActivity.kt
@@ -3,11 +3,14 @@ package com.kaz.playlistify
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.google.firebase.FirebaseApp
 import com.kaz.playlistify.ui.theme.AppNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FirebaseApp.initializeApp(this)
         setContent {
             AppNavigation()
         }
