b6c7973a3da4853cdd01c51540b7fc0559c66acf
chris <kaz71@live.com.mx>
2025-04-28 16:46:39 -0600

    ya agregamos canciones a la Queue de Firebase

--- app/src/main/java/com/kaz/playlistify/model/Cancion.kt
+++ app/src/main/java/com/kaz/playlistify/model/Cancion.kt
@@ -1,8 +1,9 @@
 package com.kaz.playlistify.model
 
 data class Cancion(
-    val id: String,             // ID del video en YouTube
-    val titulo: String,         // Título del video
-    val usuario: String,        // Quién lo agregó (nickname o ID)
-    val thumbnailUrl: String    // URL del thumbnail
+    val id: String = "",
+    val titulo: String = "",
+    val usuario: String = "",
+    val thumbnailUrl: String = "",
+    val duration: String = ""
 )
